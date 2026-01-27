23f6ca5608ab039b08b5c4a706dac5480edba58c
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2026-01-27 16:35:56 +0100

    getAllProjectById avec le prof, le projet et les étudients

--- Backend/db/dbProjectsTeachers.js
+++ Backend/db/dbProjectsTeachers.js
@@ -5,8 +5,35 @@ const dbProjectsTeachers = {
         let con;
         try {
             con = await db.connectToDatabase();
-            const sqlQuery = `SELECT * FROM projects_has_teachers`
+            const sqlQuery = `
+                SELECT  p.Name, p.Name_groupe,
+                        t.Acronyme, t.Lastname
+                FROM projects_has_teachers pht
+                JOIN Projects p ON pht.Projects_id = p.id
+                JOIN Teachers t ON pht.Teachers_id = t.id`
             const [rows] = await con.query(sqlQuery);
+            return rows;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        } finally {
+            if (con) await db.disconnectFromDatabase(con);
+        }
+    },
+    getAllProjectsById : async(projectsId) => {
+        let con;
+        try {
+            con = await db.connectToDatabase();
+            const sqlQuery = `
+                SELECT p.Name As "Nom de projet", p.Name_groupe,
+                t.Acronyme,
+                s.Firstname, s.Lastname
+                FROM Projects p
+                JOIN Projects_has_Teachers pht ON pht.Projects_id = p.id
+                JOIN Teachers t ON pht.Projects_id = t.id
+                JOIN Students s ON p.id = s.Projects_id
+                WHERE p.id = ?`
+            const [rows] = await con.query(sqlQuery, [projectsId]);
             return rows;
         } catch (error) {
             console.error(error);
@@ -16,20 +43,22 @@ const dbProjectsTeachers = {
         }
     }
 }
+
+
 /*
-const dbprojectHasTeachers = {
-    getAllTeachers: async (id) => {
+const dbProjectsTeachers = {
+    getAllProjectsTeachers : async () =>{
         let con;
         try {
             con = await db.connectToDatabase();
-            const sqlQuery = `SELECT * FROM projects_has_teachers`;
-            const [rows] = await Promise.all([]);
+            const sqlQuery = `SELECT * FROM projects_has_teachers`
+            const [rows] = await con.query(sqlQuery);
             return rows;
         } catch (error) {
             console.error(error);
             throw error;
         } finally {
-            if (con) await dbprojectHasTeachers.delete(id);
+            if (con) await db.disconnectFromDatabase(con);
         }
     }
 }
--- Backend/routes/ProjectsTeachers.js
+++ Backend/routes/ProjectsTeachers.js
@@ -12,4 +12,19 @@ projectsTeachersRouter.get("/", async (req, res) => {
     }
 })
 
+projectsTeachersRouter.get("/groupes/:id", async (req, res) => {
+    try {
+        const projectsId = req.params.id; // Récupérer le "1" de l'URL
+        const results = await dbProjectsTeachers.getAllProjectsById(projectsId);
+
+        if (results.length === 0) {
+            return res.status(404).json({ message: "Aucun projet trouvé pour cette ID." });
+        }
+
+        res.json(results);
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+});
+
 export default projectsTeachersRouter;
